152a70795a2a031efba16e940c2f55c4a70bee97
boruch d <dubywider@gmail.com>
2019-04-02 16:50:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+ if array.length == 1
+   return array.join("")
 
+
+ 
+ elsif  array.length == 2
+   return array.join(" and " )
+
+  elsif array.length == 3
+   remove = array.pop
+   array.join(", ").to_s << (", and #{remove}")
+ else
+   remove = array.pop
+   array.join(", ").to_s << (", and #{remove}")
+ end
 end

3211802a23a1f4f846a78d5bf5171b49a27971d4
boruch d <dubywider@gmail.com>
2019-04-02 16:44:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,16 @@
 def oxford_comma(array)
-  if array.length==1 
-    array.join
-  else array.length==2
-    array.join("and")
-    arry
+ if array.length == 1
+   return array.join("")
 
+
+ end 	 elsif array.length == 2
+   return array.join(" and " )
+
+  elsif array.length == 3
+   remove = array.pop
+   array.join(", ").to_s << (", and #{remove}")
+ else
+   remove = array.pop
+   array.join(", ").to_s << (", and #{remove}")
+ end
 end
